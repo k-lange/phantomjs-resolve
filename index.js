34438e4d3ff06a144a2795ee1c803f819df884d6
k-lange <mail@klange.org>
2015-11-01 17:22:42 +0100

    feat: check for specific version

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 var which = require('which');
+var cp = require('child_process');
+var semver = require('semver');
+
+var path = which.sync('phantomjs');
+var versionOutput = cp.execSync(path + ' -v', { encoding: 'utf8' });
+var versionMatch = versionOutput.match(/\d+(?:\.\d+){2}/);
+var version = versionMatch && versionMatch[0];
+
+if (!semver.satisfies(version, '2.0.x')) {
+    throw(new Error('Wrong phantomjs version', version || versionOutput));
+}
 
 module.exports = {
-    path: which.sync('phantomjs')
+    path: path
 };
